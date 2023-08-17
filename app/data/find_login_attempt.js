29ac505b4c7aab050aa6abd0ddd8537529cda894
Gabriel Bana <gabriel@init.uy>
2023-08-17 17:21:23 -0300

    Made verify signature a module.

--- app/data/find_login_attempt.js
+++ app/data/find_login_attempt.js
@@ -4,12 +4,6 @@ module.exports = function findLoginAttempt(deps) {
       .findLoginAttempt({ loginAttemptId: payload.loginAttemptId })
       .then((loginAttempt) => {
         if (!loginAttempt) return Promise.reject({ loginAttempt: "NOT_FOUND" });
-        var verified = deps.verifySignature({
-          address: payload.address,
-          signature: payload.signature,
-          loginAttempt: loginAttempt._id,
-        });
-        if (!verified) return Promise.reject({ signature: "INVALID" });
         payload.loginAttempt = loginAttempt;
         return payload;
       })
---
+++ app/verification/verify_signature.js
@@ -0,0 +1,13 @@
+module.exports = function verifySignature(deps) {
+  return function (payload) {
+    return deps.verifySignature({
+      address: payload.address,
+      signature: payload.signature,
+      loginAttempt: payload.loginAttempt._id,
+    })
+    .then((verification) => {
+      if (!verification) return Promise.reject({ signature: "INVALID" });
+      return payload;
+    })
+  }
+}
